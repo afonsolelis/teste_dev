b462404142d26f5fa500a1a2ad7e76a31f75092d
Afonso <afonsolelis@gmail.com>
2022-08-31 00:03:53 -0300

    feat: create transaction and add up to account

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,27 +1,26 @@
 class TransactionsController < ApplicationController
   before_action :set_transaction, only: %i[ show edit update destroy ]
 
-  # GET /transactions or /transactions.json
   def index
     @transactions = Transaction.all
   end
 
-  # GET /transactions/1 or /transactions/1.json
   def show
   end
 
-  # GET /transactions/new
   def new
     @transaction = Transaction.new
   end
 
-  # GET /transactions/1/edit
   def edit
   end
 
-  # POST /transactions or /transactions.json
   def create
-    @transaction = Transaction.new(transaction_params)
+    @transaction = Transaction.new(transaction_params.merge(user: current_user))
+
+    account = Account.find(@transaction.account_id)
+    account.balance += transaction_params["value"].to_i
+    account.save!
 
     respond_to do |format|
       if @transaction.save
@@ -34,7 +33,6 @@ class TransactionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /transactions/1 or /transactions/1.json
   def update
     respond_to do |format|
       if @transaction.update(transaction_params)
@@ -47,7 +45,6 @@ class TransactionsController < ApplicationController
     end
   end
 
-  # DELETE /transactions/1 or /transactions/1.json
   def destroy
     @transaction.destroy
 
@@ -58,12 +55,10 @@ class TransactionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_transaction
       @transaction = Transaction.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def transaction_params
       params.require(:transaction).permit(:account_id, :user_id, :value)
     end
